39b8de88d6b4ef36cbef1a9e86d0a561b5bff4ed
Ivan Grishaev <ivan@grishaev.me>
2019-09-02 11:08:50 +0300

    with-where macros

--- src/domic/core2.clj
+++ src/domic/core2.clj
@@ -5,7 +5,7 @@
             [honeysql.core :as sql]
 
             [domic.var-manager :as vm]
-            [domic.sql-builder :as sb]
+            [domic.query-builder :as qb]
             [domic.attr-manager :as am]
 
             ))
@@ -18,8 +18,9 @@
    :where
    [?e :artist/name ?name]
    [?r :release/artist ?e]
-   [?r :release/year ?year]
-   [(> ?year 1970)]
+   (not
+    [?r :release/year ?year]
+    [(> ?year 1970)])
    [(> ?year ?e)]
 
    ]
@@ -49,6 +50,11 @@
 
 
 
+(defn error!
+  [message & args]
+  (throw (new Exception ^String (apply format message args))))
+
+
 (defn kw->str [kw]
   (-> kw str (subs 1)))
 
@@ -65,29 +71,87 @@
       :var
       (if (vm/bound? vm arg)
         (vm/get-val vm arg)
-        (throw (new Exception "var not bound"))))))
+        (error! "Var %s is not bound" arg)))))
 
 
 (defn add-predicate
-  [expression vm sb]
+  [expression vm qb]
   (let [{:keys [expr]} expression
         {:keys [pred args]} expr
         [tag pred] pred]
-
     (case tag
       :sym
       (case pred
         (= > < >= <= != <>)
         (do
-          (assert (= (count args) 2)
-                  "A predicate takes exactly two arguments")
+          (when-not (= (count args) 2)
+            (error! "Predicate %s should take two args" pred))
           (let [[arg1 arg2] args
                 where [pred
                        (->pred-arg arg1 vm)
                        (->pred-arg arg2 vm)]]
+            (qb/add-where qb where)))))))
 
-            (sb/add-where sb where)))))))
 
+(defn add-pattern
+  [expression vm qb am]
+  (let [{:keys [elems]} expression
+        [e a v t] elems
+
+        prefix (str (gensym "d"))
+
+        attr
+        (let [[tag a] a]
+          (case tag
+            :cst
+            (let [[tag a] a]
+              (case tag
+                :kw a))))
+
+        pg-type (am/get-pg-type am attr)]
+
+    (qb/add-from qb [:datoms (keyword prefix)])
+
+    ;; E
+    (let [[tag e] e]
+      (case tag
+        :var
+        (let [sql (keyword (format "%s.e" prefix))]
+          (if (vm/bound? vm e)
+            (let [where [:= sql (vm/get-val vm e)]]
+              (qb/add-where qb where))
+            (vm/bind vm e :where sql)))))
+
+    ;; A
+    (let [where [:= (sql/raw (format "%s.a" prefix)) (kw->str attr)]]
+      (qb/add-where qb where))
+
+    ;; V
+    (let [[tag v] v]
+
+      (let [sql (sql/raw (format "%s.v::%s" prefix pg-type))]
+
+        (case tag
+          :cst
+          (let [[tag v] v]
+            (let [where [:= sql v]]
+              (qb/add-where qb where)))
+
+          :var
+          (if (vm/bound? vm v)
+            (let [where [:= sql (vm/get-val vm v)]]
+              (qb/add-where qb where))
+            (vm/bind vm v :where sql)))))))
+
+
+(defn add-clause
+  [clause vm qb am]
+  (let [[tag expression] clause]
+    (case tag
+      :pred-expr
+      (add-predicate expression vm qb)
+      :data-pattern
+      (add-pattern expression vm qb am))))
 
 
 (defn aaa
@@ -109,72 +173,29 @@
 
         vm (vm/manager)
 
-        sb (sb/builder)
+        qb (qb/builder)
 
         {:keys [find in where]} query-parsed
 
         {:keys [spec]} find
         {:keys [clauses]} where]
 
-    (doseq [clause-wrapper clauses]
+    (doseq [clause clauses]
 
-      (let [[tag clause] clause-wrapper]
+      (let [[tag clause] clause]
         (case tag
-          :expression-clause
-          (let [[tag expression] clause]
-            (case tag
 
-              :pred-expr
-              (add-predicate expression vm sb)
-
-              :data-pattern
-              (let [{:keys [elems]} expression
-                    [e a v t] elems
-
-                    prefix (str (gensym "d"))
-
-                    attr
-                    (let [[tag a] a]
-                      (case tag
-                        :cst
-                        (let [[tag a] a]
-                          (case tag
-                            :kw a))))
-
-                    pg-type (am/get-pg-type am attr)]
-
-                (sb/add-from sb [:datoms (keyword prefix)])
-
-                ;; E
-                (let [[tag e] e]
+          :not-clause
+          (qb/with-where-not-and qb
+            (let [{:keys [clauses]} clause]
+              (doseq [clause clauses]
+                (let [[tag clause] clause]
                   (case tag
-                    :var
-                    (let [sql (keyword (format "%s.e" prefix))]
-                      (if (vm/bound? vm e)
-                        (let [where [:= sql (vm/get-val vm e)]]
-                          (sb/add-where sb where))
-                        (vm/bind vm e :where sql)))))
+                    :expression-clause
+                    (add-clause clause vm qb am))))))
 
-                ;; A
-                (let [where [:= (sql/raw (format "%s.a" prefix)) (kw->str attr)]]
-                  (sb/add-where sb where))
-
-                ;; V
-                (let [[tag v] v]
-
-                  (let [sql (sql/raw (format "%s.v::%s" prefix pg-type))]
-
-                    (case tag
-                      :cst
-                      (let [[tag v] v]
-                        (let [where [:= sql v]]
-                          (sb/add-where sb where)))
-
-                      :var
-                      (if (vm/bound? vm v)
-                        (let [where [:= sql (vm/get-val vm v)]]
-                          (sb/add-where sb where))
-                        (vm/bind vm v :where sql)))))))))))
+          :expression-clause
+          (add-clause clause vm qb am))))
 
     (let [[tag] spec]
       (case tag
@@ -187,7 +208,7 @@
                 (let [[_ var] find-elem]
                   (if (vm/bound? vm var)
                     (let [val (vm/get-val vm var)]
-                      (sb/add-select sb val))
-                    (throw (new Exception "var is not bound"))))))))))
+                      (qb/add-select qb val))
+                    (error! "Var %s is not bound" var)))))))))
 
-    (sb/format sb)))
+    (qb/->map qb)))
---
+++ src/domic/query_builder.clj
@@ -0,0 +1,88 @@
+(ns domic.query-builder
+  (:refer-clojure :exclude [format])
+  (:require [honeysql.core :as sql]))
+
+
+(defprotocol IQueryBuilder
+
+  (where-stack-up [this op])
+
+  (where-stack-down [this])
+
+  (add-select [this select])
+
+  (add-from [this from])
+
+  (add-where [this where])
+
+  (->map [this])
+
+  (format [this]))
+
+
+(defmacro with-where [qb op & body]
+  `(do
+     (where-stack-up ~qb ~op)
+     (try
+       ~@body
+       (finally
+         (where-stack-down ~qb)))))
+
+
+(defmacro with-where-not [qb & body]
+  `(with-where ~qb :not ~@body))
+
+(defmacro with-where-or [qb & body]
+  `(with-where ~qb :or ~@body))
+
+(defmacro with-where-and [qb & body]
+  `(with-where ~qb :and ~@body))
+
+(defmacro with-where-not-and [qb & body]
+  `(with-where-not ~qb
+     (with-where-and ~qb
+       ~@body)))
+
+(defmacro with-where-not-or [qb & body]
+  `(with-where-not ~qb
+     (with-where-or ~qb
+       ~@body)))
+
+
+(defn builder
+  []
+  (let [where-index (atom 0)
+        where-stack (atom [(atom [:and])])
+
+        sql (atom {:select []
+                   :from []})]
+
+    (reify IQueryBuilder
+
+      (where-stack-up [this op]
+        (swap! where-index inc)
+        (swap! where-stack conj (atom [op])))
+
+      (where-stack-down [this]
+        (swap! where-index dec))
+
+      (add-select [this select]
+        (swap! sql update :select conj select))
+
+      (add-from [this from]
+        (swap! sql update :from conj from))
+
+      (add-where [this where]
+        (let [where-node (get @where-stack @where-index)]
+          (swap! where-node conj where)))
+
+      (->map [this]
+        (let [stacks (reverse (map deref @where-stack))
+              where (reduce (fn [where stack]
+                              (conj stack where))
+                            (first stacks)
+                            (rest stacks))]
+          (assoc @sql :where where)))
+
+      (format [this]
+        (sql/format (->map this))))))
